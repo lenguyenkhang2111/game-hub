a1efde5cbaff0b9d22fab885b51a4684aae45e66
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2023-03-30 22:12:04 +0700

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid padding="10px" columns={{ sm: 1, md: 2, lg: 3 }} spacing={10}>
+      <SimpleGrid padding="10px" columns={{ sm: 1, md: 2, lg: 3 }} spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
