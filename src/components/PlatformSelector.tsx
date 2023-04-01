713b8a7625cf144f13872d99bffabc12581ade63
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2023-04-01 19:34:03 +0700

    commit

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,23 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronBarDown } from "react-icons/bs";
+
+interface Props {
+  onSelectPlatform: (platform: Platform)
+}
+
+const PlatformSelector = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+        Platform
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Item 1</MenuItem>
+        <MenuItem>Item 2</MenuItem>
+        <MenuItem>Item 3</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
